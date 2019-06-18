c514337348e829e561cabcb3f265c62d37baa044
Konstantin-Bogdanoski(konstantin.b@live.com) <konstantin.b@live.com>
2019-06-18 20:22:56 +0200

    Reformat Form1 -> SpaceOddysey

--- source/VP_Project/VP_Project/Form1.Designer.cs
+++ source/VP_Project/VP_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VP_Project
 {
-    partial class Form1
+    partial class SpaceOddysey
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // SpaceOddysey
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "SpaceOddysey";
+            this.Text = "SpaceOddysey";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- source/VP_Project/VP_Project/Form1.cs
+++ source/VP_Project/VP_Project/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VP_Project
 {
-    public partial class Form1 : Form
+    public partial class SpaceOddysey : Form
     {
-        public Form1()
+        public SpaceOddysey()
         {
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Maximized;
