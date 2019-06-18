0947e08173dffee1b7b85adae993ed18a799ea88
Bojan Jovanoski <B.jovanoski@outlook.com>
2019-06-18 20:15:52 +0200

    Add background to resources

--- source/VP_Project/VP_Project/Properties/Resources.Designer.cs
+++ source/VP_Project/VP_Project/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace VP_Project.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap giphy {
+            get {
+                object obj = ResourceManager.GetObject("giphy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap HeroShip1 {
             get {
                 object obj = ResourceManager.GetObject("HeroShip1", resourceCulture);
