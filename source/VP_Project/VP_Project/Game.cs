343ecd6850509c47a1a96ec6630fce6808c45496
Konstantin Bogdanosk <konstantin.b@live.com>
2019-06-21 22:22:09 +0200

    - Add basic functions to Game.cs

--- source/VP_Project/VP_Project/Game.cs
+++ source/VP_Project/VP_Project/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,41 @@ namespace VP_Project
 {
     public class Game
     {
+        private List<Enemy> Enemies { get; set; }
+        private Hero Hero { get; set; }
+        private Boss Boss { get; set; }
+        private List<Meteor> Meteors { get; set; }
+        public Game()
+        {
+
+        }
+        public void Draw(Graphics g)
+        {
+
+        }
+        public void MoveEnemies()
+        {
+
+        }
+        public void MoveHero()
+        {
+
+        }
+        public void MoveBoss()
+        {
+
+        }
+        public void CheckBulletsImpact()
+        {
+
+        }
+        public void CheckMeteorImpact()
+        {
+
+        }
+        public void NextLevel()
+        {
+
+        }
     }
 }
