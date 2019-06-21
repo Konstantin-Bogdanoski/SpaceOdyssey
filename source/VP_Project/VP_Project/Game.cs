4b2547639d938dbec4a28f87a4f103f7285db369
Konstantin Bogdanosk <konstantin.b@live.com>
2019-06-21 22:13:06 +0200

    - Create class Game

---
+++ source/VP_Project/VP_Project/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_Project
+{
+    public class Game
+    {
+    }
+}
